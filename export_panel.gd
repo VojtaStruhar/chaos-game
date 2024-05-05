c6c192ebfa4250f20564ce866a00c087598debda
Vojtěch Struhár <vojtechstruhar@gmail.com>
2024-05-05 23:07:52 +0200

    Using the recursive algorithm for export

--- export_panel.gd
+++ export_panel.gd
@@ -3,27 +3,34 @@ extends MarginContainer
 ## Assigned by the main scene
 var preset: ChaosPreset
 
-@onready var export_button: Button = $VBoxContainer/ExportButton
-@onready var export_iterations: SpinBox = $VBoxContainer/HBoxContainer/ExportIterations
+
+@onready var export_level: SpinBox = %ExportLevel
+@onready var expected_steps_label: Label = %ExpectedStepsLabel
+@onready var export_button: Button = %ExportButton
 
 @onready var accept_dialog: AcceptDialog = $AcceptDialog
 
 func _ready() -> void:
 	visibility_changed.connect(func():
 		export_button.disabled = preset == null
+		_update_expected_steps(export_level.value)
 	)
 	
+	export_level.value_changed.connect(_update_expected_steps)
 	export_button.pressed.connect(export)
 
 func export() -> void:
 	var export_preset: ChaosPreset = preset.duplicate(true)
-	export_preset.iterations = export_iterations.value
-	export_preset.canvas_size = 4096
+	export_preset.canvas_size = 4096  # The 4K part
 	
 	var image: Image = Image.create(export_preset.canvas_size, export_preset.canvas_size, false, Image.FORMAT_RGBA8)
-	ChaosGame.run_iterative(export_preset, image)
+	ChaosGame.run_recursive(export_preset, image, export_level.value)
 	
 	var err = image.save_png("result.png")
 	print(error_string(err))
 	
 	accept_dialog.show()
+
+
+func _update_expected_steps(new_value) -> void:
+	expected_steps_label.text = str(pow(preset.points.size(), new_value))
